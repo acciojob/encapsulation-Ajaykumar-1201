56dd1d15672f26f085f531503ea4db93a921bc2a
Ajay Kumar <ajaymahto20319@gmail.com>
2023-05-12 10:04:38 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    RWOnly obj = new RWOnly();
+
+    // Set the value of "name"
+        obj.setName("John");
+
+    // Get the value of "name" and print it
+        System.out.println(obj.getName());
 }
